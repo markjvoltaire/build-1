9ac689ade968987b3cf67544005280b7ab447bfa
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-08 17:25:10 -0400

    generate button generates a random number

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
+const getNumber = document.getElementById('generate');
+const results = document.getElementById('random-num');
 
 // initialize global state
+const randomFunction = Math.floor(Math.random() * 100) + 1;
 
-// set event listeners 
+// set event listeners
+getNumber.addEventListener('click', ()=>{
+    results.textContent = randomFunction;
+});
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
